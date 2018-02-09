a91938867c65c343ba44d01113045c8b1dfe90de
Daniel Hunter <officialdanielhunter@gmail.com>
2018-02-09 17:20:03 +0000

    Add autoscript file

---
+++ autoscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# touch test
+# roscore
+# source /home/ubuntu/.bashrc
+source /opt/ros/kinetic/setup.bash
+source /home/ubuntu/catkin_ws/devel/setup.bash
+source /etc/ubuquity/env.sh
+#roslaunch beginner_tutorials base.launch
+# rosrun beginner_tutorials listener.py & rosrun beginner_tutorials controller.py 
