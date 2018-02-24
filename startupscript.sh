390b0c9fc510dab122fd0fcb657f1b75820b3799
Daniel Hunter <officialdanielhunter@gmail.com>
2018-02-24 17:53:18 +0000

    Small updates to the keyboard listener, removed server from launch file

--- startupscript.sh
+++ startupscript.sh
@@ -5,6 +5,6 @@
 # source /home/ubuntu/.bashrc
  source /opt/ros/kinetic/setup.bash
  source /home/ubuntu/catkin_ws/devel/setup.bash
-#source /etc/ubuquity/env.sh
+# source /etc/ubuquity/env.sh
 # roslaunch ians_project base.launch >> ~/log 2>> ~/errorLog
 # rosrun beginner_tutorials listener.py & rosrun beginner_tutorials controller.py 
