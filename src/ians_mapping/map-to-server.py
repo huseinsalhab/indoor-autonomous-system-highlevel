5ad729efababf6996cdba634ed5607820f3f6363
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-14 03:26:17 +0000

    Fix map-to-server, filename must be hardcoded

--- src/ians_mapping/map-to-server.py
+++ src/ians_mapping/map-to-server.py
@@ -6,17 +6,14 @@ server_ip = 'http://35.229.88.91'
 endpoint = '/v1/robot_receive_map'
 
 png_name = '/home/ubuntu/indoor-autonomous-system-highlevel/src/ians_mapping/map.png'
-png_name = '/home/ubuntu/indoor-autonomous-system-highlevel/src/ians_mapping/random_detail.jpg'
-# png_name = 'random_detail.jpg'
 
-def post_image(data):
-    """ Finds the map .pgm file, converts to .png, and sends it to the specified
+def main():
+    """ Finds the map .png and sends it to the specified
     server endpoint
     """
     # POST map image to server
     with open(png_name, 'rb') as f:
-        r = requests.post(server_ip + endpoint, files={png_name: f})
-        # r = requests.post('https://requestb.in/1ltofds1', files={png_name: f})
+        r = requests.post(server_ip + endpoint, files={'map.png': f})
 
 if __name__ == '__main__':
-    post_image('blah')
+    main()
