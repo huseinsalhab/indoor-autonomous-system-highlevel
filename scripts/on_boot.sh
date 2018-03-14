aa2b0fe256c4c23c51f234999a306b043f99d865
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-14 15:45:54 -0700

    Update on_boot.sh

--- scripts/on_boot.sh
+++ scripts/on_boot.sh
@@ -4,12 +4,10 @@
 source /opt/ros/kinetic/setup.bash
 source /home/ubuntu/indoor-autonomous-system-highlevel/devel/setup.bash
 
-# Wait for the RPLidar node to fully boot before calling stop motor
-
 roslaunch ians_control ians_ctl.launch & # Spin up Motors and Keyboard
 
 /home/ubuntu/indoor-autonomous-system-highlevel/src/ians_control/server_interface.out
 
-sleep 10
+sleep 10 # Wait for the RPLidar node to fully boot before calling stop motor
 rosservice call stop_motor # Stop LiDAR spinning
 # roslaunch ians_project base.launch >> ~/log 2>> ~/errorLog
