d930f41afddbab3b6e13d98394ed5e4a8d47a970
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-14 02:51:27 +0000

    More updates to stop mapping, I think I need to use a python file as well

--- scripts/stop_mapping.sh
+++ scripts/stop_mapping.sh
@@ -7,13 +7,10 @@ map_path="/home/ubuntu/indoor-autonomous-system-highlevel/src/ians_mapping/"
 # rosrun map_server map_saver -f /home/ubuntu/indoor-autonomous-system-highlevel/src/ians_mapping/map.pgm
 
 # convert the newly created .pgm file to be a .png for the web server
-convert "$map_path"map.pgm "$map_path"map.png
+# convert "$map_path"map.pgm "$map_path"map.png
 
 # POST the .png file to the web server
-curl \
--F "filecomment=This is an image file" \
--F "image=@""$map_path""map.png" \
-https://requestb.in/1gyws5q1
+python "$map_path"map-to-server.py
 
 # rosrun ians_mapping map-to-server.py
 # rostopic pub /map_upload std_msgs/String "data: 'post'"
--- src/ians_mapping/map-to-server.py
+++ src/ians_mapping/map-to-server.py
@@ -1,42 +1,22 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
 import requests
 from subprocess import call
-
 
 server_ip = 'http://35.229.88.91'
 endpoint = '/v1/robot_receive_map'
 
-pgm_name = 'map.pgm'
-png_name = 'map.png'
+png_name = '/home/ubuntu/indoor-autonomous-system-highlevel/src/ians_mapping/map.png'
+png_name = '/home/ubuntu/indoor-autonomous-system-highlevel/src/ians_mapping/random_detail.jpg'
+# png_name = 'random_detail.jpg'
 
-subscribe_topic = 'map_upload'
-
-def callback(data):
+def post_image(data):
     """ Finds the map .pgm file, converts to .png, and sends it to the specified
     server endpoint
     """
-    if data.data == 'post':
-        # Convert the pgm file to png file
-        call(["convert", pgm_name, png_name])
-
-        # POST map image to server
-        with open(png_name, 'rb') as f:
-            r = requests.post(server_ip + endpoint, files={filename: f})
-
-def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber(subscribe_topic, String, callback)
-
-    rospy.spin() # spin() simply keeps python from exiting until this node is stopped
+    # POST map image to server
+    with open(png_name, 'rb') as f:
+        r = requests.post(server_ip + endpoint, files={png_name: f})
+        # r = requests.post('https://requestb.in/1ltofds1', files={png_name: f})
 
 if __name__ == '__main__':
-    listener()
+    post_image('blah')
