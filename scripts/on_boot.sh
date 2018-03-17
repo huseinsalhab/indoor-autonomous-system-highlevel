3dfa727e1c3bc04be7bfdafc3bc1da3fd850b7df
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-17 00:31:26 +0000

    Clean up on boot script, run server interface in background

--- scripts/on_boot.sh
+++ scripts/on_boot.sh
@@ -6,8 +6,7 @@ source /home/ubuntu/indoor-autonomous-system-highlevel/devel/setup.bash
 
 roslaunch ians_control ians_ctl.launch & # Spin up Motors and Keyboard
 
-/home/ubuntu/indoor-autonomous-system-highlevel/src/ians_control/server_interface.out
+/home/ubuntu/indoor-autonomous-system-highlevel/src/ians_control/server_interface.out &
 
-sleep 10 # Wait for the RPLidar node to fully boot before calling stop motor
+sleep 7 # Wait for the RPLidar node to fully boot before calling stop motor
 rosservice call stop_motor # Stop LiDAR spinning
-# roslaunch ians_project base.launch >> ~/log 2>> ~/errorLog
