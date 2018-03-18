c865b49fba1b3a3a605a43ef4849190a17e49a1a
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-17 19:29:07 -0700

    Update keyboard_listener.py

--- src/ians_control/keyboard_listener.py
+++ src/ians_control/keyboard_listener.py
@@ -5,8 +5,11 @@
 
 import sys
 import rospy
-from std_msgs.msg import Float32 
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Twist
 import pygame
+
+last_char = ''
 
 class KeyboardListener(object):
     """Class representing the keyboard listener."""
@@ -17,60 +20,47 @@ class KeyboardListener(object):
         pygame.display.set_caption(u'Keyboard events')
         pygame.display.set_mode((400, 400))
 
-    def listen(self, r_callback, l_callback):
+    def listen(self, callback):
         """Listen for events to happen and passes the pressed character
         + an indicator of key press vs key release into the callback function"""
-        fwd_speed = 75.0
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    try:
-                        real_char = chr(event.key)
-                        if real_char == 'w':
-                            # Publish L + R fwd
-                            r_callback(fwd_speed)
-                            l_callback(fwd_speed)
-                        if real_char == 's':
-                            # Publish L + R back 
-                            r_callback(fwd_speed * -1)
-                            l_callback(fwd_speed * -1)
-                        if real_char == 'd':
-                            # Turn right
-                            r_callback(fwd_speed * -1)
-                            l_callback(fwd_speed)
-                        if real_char == 'a':
-                            # Turn left
-                            r_callback(fwd_speed)
-                            l_callback(fwd_speed * -1)
-                        if real_char == 'x':
-                            # Emergency brake 
-                            r_callback(0)
-                            l_callback(0)
-                    except:
-                        pass
-                elif event.type == pygame.KEYUP:
-                    try:
-                        # Turn off all motors if keys are released
-                        r_callback(0)
-                        l_callback(0)
-                    except:
-                        pass
-                elif event.type == pygame.QUIT:
-                    pygame.display.quit()
-                    pygame.quit()
-                    sys.exit()
-
+        
+        ## Get any new events before publishing to ROS
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                last_char = chr(event.key)
+            elif event.type == pygame.KEYUP:
+                last_char = ''
+            elif event.type == pygame.QUIT:
+                pygame.display.quit()
+                pygame.quit()
+                sys.exit()
+        ## Publish to geo twist topic based on whatever key was last pressed
+        msg = Twist()
+        if last_char == 'w':
+            # Drive forward
+            msg.linear.x = 10
+        if last_char == 's':
+            # Drive backward
+            msg.linear.x = -10
+        if last_char == 'd':
+            # Turn right
+            msg.angular.z = -1
+        if last_char == 's':
+            # Turn left
+            msg.angular.z = 1
+        # #Publish the message after creating the struct
+        callback(msg)    
+                                                   
 def keyboard_event_publisher():
     """Listens for keyboard events and publishes them to the specified topic"""
     key_capture = KeyboardListener()
 
-    right_pub = rospy.Publisher('rmotor', Float32, queue_size = 10)
-    left_pub = rospy.Publisher('lmotor', Float32, queue_size = 10)
+    geo_twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
     rospy.init_node('keyboard_event_publisher', anonymous=True)
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-       key_capture.listen(right_pub.publish, left_pub.publish)
+       key_capture.listen(geo_twist_pub.publish)
        rate.sleep()
 
 if __name__ == '__main__':
@@ -78,4 +68,3 @@ if __name__ == '__main__':
         keyboard_event_publisher()
     except rospy.ROSInterruptException:
         pass
-
