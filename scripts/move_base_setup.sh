3f7186d17f237dd1b409124b668f65036bb8376b
Daniel Hunter <officialdanielhunter@gmail.com>
2018-04-14 13:58:06 -0700

    Updated cost map params, added move base shell script

---
+++ scripts/move_base_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script assumes launch_everything.sh has already been run
+rosservice call start_motor
+
+rosrun map_server map_server \
+/home/ubuntu/indoor-autonomous-system-highlevel/src/ians_mapping/301-test-apr6.yaml &
+
+roslaunch ians_navigation navstack_standup.launch & 
