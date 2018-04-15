e19e106a908e3328252affb0896b06334315bbac
kebding <kebding@gmail.com>
2018-04-14 19:48:57 -0700

    first successfully compiling version

--- src/ians_control/set_nav_goal.cpp
+++ src/ians_control/set_nav_goal.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <stdio.h>
 
 /* This ROS node is launched by a shell script in response to receiving a new 
    destination from the server. This node takes in the command line arguments 
@@ -10,31 +11,32 @@
 
 */
 
+#define NUM_ARGS 5
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 
 int main(int argc, char** argv){
     // parse command line arguments. assumes command was called with: 
     // "rosrun ians_control set_nav_goal x_dest y_dest x_start y_start"
-    if (argc < 7) {
+    if (argc < NUM_ARGS) {
         printf("error: invalid command line arguments\n");
         exit(-1);
     }
-    int x_dest, y_dest, x_start, y_start;
+    int x_dest, y_dest; 
     sprintf(argv[3], "%d", x_dest);
     sprintf(argv[4], "%d", y_dest);
-    sprintf(argv[5], "%d", x_start);
-    sprintf(argv[6], "%d", x_start);
 
     ros::init(argc, argv, "set_nav_goal");
 
     //tell the action client that we want to spin a thread by default
     MoveBaseClient ac("move_base", true);
 
+    /*
     //wait for the action server to come up
     while(!ac.waitForServer(ros::Duration(5.0))){
         ROS_INFO("Waiting for the move_base action server to come up");
-    }
+    } */
     move_base_msgs::MoveBaseGoal goal;
 
     goal.target_pose.header.frame_id = "base_link";
@@ -44,13 +46,11 @@ int main(int argc, char** argv){
     goal.target_pose.pose.position.y = y_dest;
     goal.target_pose.pose.orientation.w = 1.0;
 
-    goal.base_position.pose.position.x = x_start;
-    goal.base_position.pose.position.y = y_start;
-    goal.base_position.pose.orientation.w = 1.0;
 
     ROS_INFO("Sending goal");
     ac.sendGoal(goal);
 
+    /*
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
         ROS_INFO("Goal successfully sent to move_base\n");
         exit(0);
@@ -59,6 +59,7 @@ int main(int argc, char** argv){
         ROS_INFO("move_base error\n");
         exit(-1);
     }
+    */
     //just in case the program reaches this point, exit
     return 0;
 }
