99c018eb65ea3869d495152abd12774cebf35e92
kebding <kebding@gmail.com>
2018-04-21 14:56:47 -0700

    add comment describing source of covariance matrix

--- scripts/set_initial_pose.sh
+++ scripts/set_initial_pose.sh
@@ -16,6 +16,5 @@ fi
 source /opt/ros/kinetic/setup.bash
 source /home/ubuntu/indoor-autonomous-system-highlevel/devel/setup.bash
 
-#rosrun ians_control set_nav_goal $1 $2
-
+# covariance matrix copied from output of setting initial pose in rviz
 rostopic pub --once /initialpose geometry_msgs/PoseWithCovarianceStamped '{header: {stamp: now, frame_id: "map"}, pose: {pose: {position: {x: '$1', y: '$2'}, orientation: {x: 0, y: 0, z: '$3', w: 1}}, covariance: [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]}}'
