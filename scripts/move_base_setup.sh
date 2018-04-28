ed168b31b9a882052752c304fcedc327cf02aaf8
Daniel Hunter <officialdanielhunter@gmail.com>
2018-04-28 15:39:19 -0700

    kelvin progress

--- scripts/move_base_setup.sh
+++ scripts/move_base_setup.sh
@@ -6,4 +6,4 @@ rosservice call start_motor
 rosrun map_server map_server \
 /home/ubuntu/indoor-autonomous-system-highlevel/src/ians_mapping/301-test-apr6.yaml &
 
-roslaunch ians_navigation navstack_standup.launch & 
+roslaunch ians_navigation navstack_standup.launch 
